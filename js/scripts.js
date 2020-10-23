370c7e79509bf77bfe533de1d50b35fc59e8c2f6
Bess Campbell <bess.k.campbell@gmail.com>
2020-10-23 16:22:41 -0700

    Add some styling to the text/background and spacing on radio buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
     const quest1= $("input:radio[name=destination]:checked").val();
     const quest2= $("input:radio[name=animal]:checked").val();
     const quest3= $("input:radio[name=activity]:checked").val();
-    const quest4= $("#year-range").val();
-    const quest5= $("#color").val();
+    // const quest4= $("#year-range").val();
+    // const quest5= $("#color").val();
     // console.log(quest1, quest2, quest3, quest4, quest5);
     
     if (quest1==="a" && quest2==="a" && quest3==="a" || quest1==="a" && quest2==="a" || quest1==="a" && quest3==="a" || quest2==="a" && quest3==="a"){
       const ruby= "you would do well learning Ruby. You like to be productive but have fun!";
-      $("#output").show();
+      $("#output").toggle();
       $("#prog-lang-answer").text(ruby);
       $("img").hide();
       $("#ruby").toggle();
@@ -19,7 +19,7 @@ $(document).ready(function(){
     }
     else if(quest1==="b" && quest2==="b" && quest3==="b" || quest1==="b" && quest2==="b" || quest1==="b" && quest3==="b" || quest2==="b" && quest3==="b"){
       const python= "you would do well learning Python. You think beautiful is better than ugly. Explicit is better than implicit. Simple is better than complex. Complex is better than complicated.";
-      $("#output").show();
+      $("#output").toggle();
       $("#prog-lang-answer").text(python);
       $(".logo").hide();
       $("#python-logo").toggle();
@@ -28,7 +28,7 @@ $(document).ready(function(){
     }
     else if (quest1==="C" && quest2==="c" && quest3==="c" || quest1==="c" && quest2==="c"|| quest1==="c" && quest3==="c" || quest2==="c" && quest3==="c"){
       const go= "you would do well learning Go. You are efficient and high-performing.";
-      $("#output").show();
+      $("#output").toggle();
       $("#prog-lang-answer").text(go);
       $(".logo").hide();
       $("#go").toggle();
@@ -37,7 +37,7 @@ $(document).ready(function(){
     }
     else if (quest1==="d" && quest2==="d" && quest3==="d" || quest1==="d" && quest2==="d" || quest1==="d" && quest3==="d" || quest2==="d" && quest3==="d"){
       const rust= "you would do well learning Rust. You are a performer at heart, with safety in mind. Above all else, you are the loved the most!";
-      $("#output").show();
+      $("#output").toggle();
       $("#prog-lang-answer").text(rust);
       $(".logo").hide();
       $("#rust").toggle();
@@ -56,9 +56,10 @@ $(document).ready(function(){
     }
   });
 });
-
 $(document).ready(function(){
   $(".clickable").click(function(){
-    $("#language").show();
+    $("#language").show().trigger("reset");
+    $("#output").toggle();
   });
 });
+//Can this function live inside the other (document).ready function?//
