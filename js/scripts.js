940629796f03759c9dda8917fb6a57d3baaf0e0d
Bess Campbell <bess.k.campbell@gmail.com>
2020-10-23 16:47:14 -0700

    Add a few small styling changes.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,7 @@ $(document).ready(function(){
     const quest1= $("input:radio[name=destination]:checked").val();
     const quest2= $("input:radio[name=animal]:checked").val();
     const quest3= $("input:radio[name=activity]:checked").val();
-    // const quest4= $("#year-range").val();
-    // const quest5= $("#color").val();
-    // console.log(quest1, quest2, quest3, quest4, quest5);
-    
+   
     if (quest1==="a" && quest2==="a" && quest3==="a" || quest1==="a" && quest2==="a" || quest1==="a" && quest3==="a" || quest2==="a" && quest3==="a"){
       const ruby= "you would do well learning Ruby. You like to be productive but have fun!";
       $("#output").toggle();
