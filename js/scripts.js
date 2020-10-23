22098b20b291b93674d450412ecc36f7c0519ad5
Bess Campbell <bess.k.campbell@gmail.com>
2020-10-23 11:59:37 -0700

    Add a container and p-tag for the output and the first branch for finding the results in scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
   $("#language").submit(function(event){
     event.preventDefault();
-    let quest1, quest2, quest3, quest4, quest5;
-    quest1= $("input:radio[name=destination]:checked").val();
-    quest2= $("input:radio[name=animal]:checked").val();
-    quest3= $("input:radio[name=activity]:checked").val();
-    quest4= $("#year-range").val();
-    quest5= $("#color").val();
+    const quest1= $("input:radio[name=destination]:checked").val();
+    const quest2= $("input:radio[name=animal]:checked").val();
+    const quest3= $("input:radio[name=activity]:checked").val();
+    const quest4= $("#year-range").val();
+    const quest5= $("#color").val();
     console.log(quest1, quest2, quest3, quest4, quest5);
+    if (quest1==="a" && quest2==="a" && quest3==="a" || quest1==="a" && quest2==="a" && quest4==="a" || quest1==="a" && quest3==="a" && quest4==="a" || quest2==="a" && quest3==="a" && quest4==="a"){
+      const ruby= "Ruby";
+      $(".output").show();
+      $("#prog-lang-answer").text(ruby);
+
+    }
   });
 });
