18e1220a5bbaca5eee18df590dee48a1ddc5d545
Bess Campbell <bess.k.campbell@gmail.com>
2020-10-23 13:29:45 -0700

    Change the branching to only take into account 3 out of the 5 questions for the output.

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,36 @@ $(document).ready(function(){
     const quest4= $("#year-range").val();
     const quest5= $("#color").val();
     console.log(quest1, quest2, quest3, quest4, quest5);
-    if (quest1==="a" && quest2==="a" && quest3==="a" || quest1==="a" && quest2==="a" && quest4==="a" || quest1==="a" && quest3==="a" && quest4==="a" || quest2==="a" && quest3==="a" && quest4==="a"){
-      const ruby= "Ruby";
-      $(".output").show();
+    
+    if ((quest1==="a" && quest2==="a" || quest1==="a" && quest3==="a" || quest1==="a" && quest4==="a" || quest2==="a" && quest3==="a" || quest2==="a" && quest4==="a" || quest3==="a" && quest4==="a")&&(quest1==="b" && quest2==="b" || quest1=="b" && quest3==="b" || quest1==="b" && quest4==="b" || quest2==="b" && quest3==="b" || quest2==="b" && quest4==="b" || quest3==="b" && quest4==="b")){
+      const rubyPython= "Ruby or Python";
+      $("#output").show();
       $("#prog-lang-answer").text(ruby);
     }
     else if(quest1==="b" && quest2==="b" && quest3==="b" || quest1==="b" && quest2==="b" && quest4==="b" || quest1==="b" && quest3==="b" && quest4==="b" || quest2==="b" && quest3==="b" && quest4==="b"){
       const python= "Python";
-      $(".output").show();
+      $("#output").show();
       $("#prog-lang-answer").text(python);
     }
-    else if (quest1==="C" && quest2==="c" && quest3==="c" || quest1==="c" && quest2==="c" && quest4==="c" || quest1==="c" && quest3==="c" && quest4==="c" || quest2==="c" && quest3==="c" && quest4==="c"){
-      const go= "Go";
-      $(".output").show();
-      $("#prog-lang-answer").text(go);
-    }
-    else if (quest1==="d" && quest2==="d" && quest3==="d" || quest1==="d" && quest2==="d" && quest4==="d" || quest1==="d" && quest3==="d" && quest4==="d" || quest2==="d" && quest3==="d" && quest4==="d"){
-      const rust= "Rust";
-      $(".output").show();
-      $("#prog-lang-answer").text(rust);
-    }
+    // else if (quest1==="C" && quest2==="c" && quest3==="c" || quest1==="c" && quest2==="c" && quest4==="c" || quest1==="c" && quest3==="c" && quest4==="c" || quest2==="c" && quest3==="c" && quest4==="c"){
+    //   const go= "Go";
+    //   $("#output").show();
+    //   $("#prog-lang-answer").text(go);
+    // }
+    // else if (quest1==="d" && quest2==="d" && quest3==="d" || quest1==="d" && quest2==="d" && quest4==="d" || quest1==="d" && quest3==="d" && quest4==="d" || quest2==="d" && quest3==="d" && quest4==="d"){
+    //   const rust= "Rust";
+      // $("#output").show();
+      // $("#prog-lang-answer").text(rust);
+    // }
   });
 });
+
+// if (quest1==="a" && quest2==="a" && quest3==="a" || quest1==="a" && quest2==="a" && quest4==="a" || quest1==="a" && quest3==="a" && quest4==="a" || quest2==="a" && quest3==="a" && quest4==="a"){
+//   const ruby= "Ruby";
+//   $("#output").show();
+//   $("#prog-lang-answer").text(ruby);
+// }
+// else if(quest1==="b" && quest2==="b" && quest3==="b" || quest1==="b" && quest2==="b" && quest4==="b" || quest1==="b" && quest3==="b" && quest4==="b" || quest2==="b" && quest3==="b" && quest4==="b"){
+//   const python= "Python";
+//   $("#output").show();
+//   $("#prog-lang-answer").text(python);
